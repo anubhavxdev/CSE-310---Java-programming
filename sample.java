2e6822eebb5d7d24e403a674d7fa41ca335d5309
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2025-01-14 12:17:32 +0530

    Refactor sample class and add square root calculation

--- sample.java
+++ sample.java
@@ -1,5 +1,16 @@
-class sample {
+// class sample {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!"); 
+//     }
+// }
+
+
+class smaple{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        double x = 4;
+        double y;
+        System.out.println("x = " + x);
+        y = Math.sqrt(x);
     }
 }
