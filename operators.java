8c4eeeb32871d968b38332ac42fb17338a9c61fd
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2025-01-27 11:49:03 +0530

    Add operators class to demonstrate arithmetic, logical, and relational operations in Java

---
+++ operators.java
@@ -0,0 +1,69 @@
+/**
+ * The operators class demonstrates the use of various arithmetic operators in Java.
+ * It performs addition, subtraction, multiplication, division, and modulus operations
+ * on integer variables and prints the results to the console.
+ * 
+ * The following operations are demonstrated:
+ * - Addition: a + b
+ * - Subtraction: a - b
+ * - Multiplication: a * b
+ * - Division: b / a
+ * - Modulus: b % a, c % a
+ * - Post-increment: a++
+ * - Post-decrement: a--
+ * - Post-increment: d++
+ * - Pre-increment: ++d
+ * 
+ * The results of these operations are printed to the console.
+ */
+public class operators {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        
+        System.out.print("Enter value for a: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Enter value for b: ");
+        int b = scanner.nextInt();
+        
+        System.out.print("Enter value for c: ");
+        int c = scanner.nextInt();
+        
+        System.out.print("Enter value for d: ");
+        int d = scanner.nextInt();
+        
+        scanner.close();
+        System.out.println("a + b = " + (a + b) );
+        System.out.println("a - b = " + (a - b) );
+        System.out.println("a * b = " + (a * b) );
+        System.out.println("b / a = " + (b / a) );
+        System.out.println("b % a = " + (b % a) );
+        System.out.println("c % a = " + (c % a) );
+        System.out.println("a++   = " +  (a++) );
+        System.out.println("b--   = " +  (a--) );
+        System.out.println("d++   = " +  (d++) );
+        System.out.println("++d   = " +  (++d) );
+
+        // Ternary operator to find the maximum of a and b
+        int max = (a > b) ? a : b;
+        System.out.println("The maximum of a and b is: " + max);
+
+
+        // Logical operators
+        boolean andResult = (a > b) && (c > d);
+        boolean orResult = (a > b) || (c > d);
+        boolean notResult = !(a > b);
+
+        System.out.println("Logical AND (a > b && c > d) = " + andResult);
+        System.out.println("Logical OR (a > b || c > d) = " + orResult);
+        System.out.println("Logical NOT !(a > b) = " + notResult);
+
+        // Relational operators
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a >= b = " + (a >= b));
+        System.out.println("a <= b = " + (a <= b));
+    }
+}
