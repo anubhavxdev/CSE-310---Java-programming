1f8eb54ddc0b91030ec46638440cd8a73cb7f200
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2025-02-11 12:34:40 +0530

    Refactor thissss class to include a default constructor and add getInstance method for object retrieval

--- thissss.java
+++ thissss.java
@@ -1,16 +1,60 @@
+// public class thissss {
+//     int a;
+//     int b;
+
+//     thissss() {
+//         this(0, 0); 
+//     }
+
+//     thissss(int a, int b) {
+//         this.a = a;
+//         this.b = b;
+//     }
+
+//     void display() {
+//         System.out.println("a = " + a + " b = " + b);
+//     }
+
+//     public static void main(String[] args) {
+//         thissss obj1 = new thissss(10, 20);
+//         obj1.display();
+
+//         thissss obj2 = new thissss(); 
+//         obj2.display();
+//     }
+// }
+
+
+
 public class thissss {
     int a;
     int b;
-    
-    thissss(int a, int b){
+
+    thissss() {
+        this(0, 0); 
+    }
+
+    thissss(int a, int b) {
         this.a = a;
         this.b = b;
     }
-    void display(){
-        System.out.println("a = "+a+" b = "+b);
+
+    thissss getInstance() {
+        return this;
     }
+
+    void display() {
+        System.out.println("a = " + a + " b = " + b);
+    }
+
     public static void main(String[] args) {
-        thissss obj = new thissss(10, 20);
-        obj.display();
+        thissss obj1 = new thissss(10, 20);
+        obj1.display();
+
+        thissss obj2 = new thissss(); 
+        obj2.display();
+
+        thissss obj3 = obj1.getInstance();
+        obj3.display();
     }
 }
