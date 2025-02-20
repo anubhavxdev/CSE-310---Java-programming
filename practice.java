8e016221d7614a81886bae25c3e57af9d15d7a9b
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2025-02-20 15:48:44 +0530

    Add subclasses SubClass1 and SubClass2 to enhance functionality and demonstrate inheritance

--- practice.java
+++ practice.java
@@ -10,7 +10,7 @@ public class practice {
     }
 
     public practice(String value) {
-        this.value = Integer.parseInt(value);
+        this.value = Integer.parseInt(value);05
     }
 
     public void display() {
@@ -33,5 +33,35 @@ public class practice {
         obj1.display();
         obj2.display("Current value");
         obj3.display(5);
+
+        SubClass1 subObj1 = new SubClass1(30);
+        subObj1.display();
+        subObj1.display("SubClass1 value");
+
+        SubClass2 subObj2 = new SubClass2("40");
+        subObj2.display();
+        subObj2.display(10);
+    }
+}
+
+class SubClass1 extends practice {
+    public SubClass1(int value) {
+        super(value);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("SubClass1 Value: " + super.value);
+    }
+}
+
+class SubClass2 extends practice {
+    public SubClass2(String value) {
+        super(value);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("SubClass2 Value: " + super.value);
     }
 }
